77e1e2ebf5d171ed309fe382ea190488e6a47410
Brian Forbes <brianandrewf81@gmail.com>
2021-05-11 22:35:07 -0600

    Create paddle A and display in terminal

--- Pong1.py
+++ Pong1.py
@@ -7,8 +7,25 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle B
+paddle_b = turtle.Turtle()
+
+
+
+#Ball
+
+
+
+
 # Main Game Loop
 
 while True:
     wn.update()
-    
